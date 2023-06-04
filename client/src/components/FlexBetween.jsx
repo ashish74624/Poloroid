c5c66f9dc497b4e547a9c15118cf16f7f0e55cf5
Ashish <ashish74624@gmail.com>
2023-06-04 14:31:10 +0530

    Navbar created

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import {Box} from '@mui/system'
+import {styled} from '@mui/system'
+
+const FixedBetween = styled(Box)({
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+})
+
+export default FixedBetween;
