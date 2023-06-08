6776e15125e81f75aa55553d22941d7aba0fe9e1
Ashish <ashish74624@gmail.com>
2023-06-08 23:37:05 +0530

    Switched to next 13

--- client/src/state2/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import valueChanger from './valueChanger';
-
-const Reducers = combineReducers({
-    //This is like making the states
-    value: valueChanger,
-    //amount is a state & amountReducer is used to change this state(like setState) 
-})
-export default Reducers
