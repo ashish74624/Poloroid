65d3e3448cbbc3edfee87108efc19f79225a727c
Ashish <ashish74624@gmail.com>
2023-06-08 23:37:05 +0530

    Switched to next 13

---
+++ client/app/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className='w-screen h-[10vh] bg-[#ECE3E1]'>
+
+      </nav>
+    </>
+  )
+}
--- client/src/state2/Store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
-import thunk from 'redux-thunk';
-import Reducers from './reducers/index'
-export const Store = createStore(Reducers, {}, applyMiddleware(thunk))
-//createStore(Reducers,{Intial State},applyMiddleware(thunk))
