28c21a77d11f677bc2f58d116db52df66262ada0
Ashish <ashish74624@gmail.com>
2023-06-09 23:39:56 +0530

    Poster was made as an seperate component

--- client/app/Components/Poster.tsx
+++ client/app/Components/Poster.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Poster() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <div className= 'relative h-screen w-[45vw]' >
+            <Image className=' opacity-60 absolue inset-0 h-screen w-[45vw]' src={'/stay.jpg'} alt='Image' width={100} height={100} />    
+            <div className='  absolue inset-0 h-screen w-[45vw] bg-purple-900' ></div>
+        </div>   
+    </>
   )
 }
