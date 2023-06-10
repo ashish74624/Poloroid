9fef32fd14d1f1f6a7947e21845c12da17bc6cbc
Ashish <ashish74624@gmail.com>
2023-06-10 23:29:28 +0530

    Added Login link to register page

--- client/app/Components/Poster.tsx
+++ client/app/Components/Poster.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import Logo from './Logo'
 
 export default function Poster() {
   return (
     <>
-     <div className= 'relative h-screen w-[45vw]' >
-            <Image className=' opacity-60 absolue inset-0 h-screen w-[45vw]' src={'/stay.jpg'} alt='Image' width={100} height={100} />    
-            <div className='  absolue inset-0 h-screen w-[45vw] bg-purple-900' ></div>
-        </div>   
+     <main className= 'relative h-screen w-[45vw] bg-purple-400' >
+        <div className='h-[20vw] w-[20vw]'>
+          <Logo/>
+        </div>
+     </main>   
     </>
   )
 }
