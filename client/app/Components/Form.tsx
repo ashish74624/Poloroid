85a50a5a7fd975f3b21e6760281085267884c50f
Ashish <ashish74624@gmail.com>
2023-06-16 22:28:28 +0530

    Drag & drop added to register page

--- client/app/Components/Form.tsx
+++ client/app/Components/Form.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
+import DropZone from './DropZone';
 
 export default function Form() {
 
@@ -116,11 +117,11 @@ export default function Form() {
                     <input type="password" name="password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-purple-600 peer" placeholder=" " required />
                     <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
-                <div className="relative z-0 w-full mb-6 group">
-                    <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-purple-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-                </div>
-                <div className='flex w-full justify-between'>
+                {/* <label className="block mb-2 text-base  font-medium text-gray-500" htmlFor="file_input">Upload Profile Picture</label>
+                <input name ='profileImage' className="block w-full text-sm text-gray-500 border border-gray-300 rounded-lg cursor-pointer  bg-[#efd3cd] focus:outline-none" id="file_input" type="file" 
+                accept="image/jpeg, image/png , image/jpg"/> */}
+                <DropZone/>
+                <div className='flex w-full justify-between pt-5'>
                     <button type="submit" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
                     
                     <button className='text-purple-400 font-semibold text-lg hover:underline'
@@ -128,11 +129,6 @@ export default function Form() {
                     >Login</button>
                 
                 </div>
-            
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
-                <input name ='profileImage' className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none" id="file_input" type="file" 
-                accept="image/jpeg, image/png , image/jpg"/>
-
             </form>
         </>)
     }
