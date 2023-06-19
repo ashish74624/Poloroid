cf4bfa2d324bb3bd2f94284de5945a70b6b1ebdb
Ashish <ashish74624@gmail.com>
2023-06-19 23:46:02 +0530

    Links to the login/register page added

---
+++ client/app/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Comfortaa } from 'next/font/google'
+
+const Comf = Comfortaa({
+    subsets:['cyrillic'],
+    weight:'400'
+  })
+
+export default function Register() {
+  return (
+    <></>
+  )
+}
