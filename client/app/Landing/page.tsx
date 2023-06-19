816be5abe4566b3e90ae84c9a2cae63b3962afa8
Ashish <ashish74624@gmail.com>
2023-06-19 23:46:02 +0530

    Links to the login/register page added

--- client/app/Landing/page.tsx
+++ client/app/Landing/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Comfortaa } from 'next/font/google'
+import Link from 'next/link'
 
 const Comf = Comfortaa({
     subsets:['cyrillic'],
@@ -14,8 +15,10 @@ export default function Landing() {
         <h1 className={`${Comf.className} text-8xl text-[#f5f5f7]`}>polaroid</h1>
         <p className='text-[#f5f5f7] text-lg'>Where your memories are always in focus</p>
         <div>
-            <button className='bg-[#f5f5f7] w-max h-10 px-4  rounded-lg mx-3 text-[#71b1d1]  hover:bg-gray-300'>Login</button>
-            <button className='bg-[#f5f5f7] w-max h-10 px-4  rounded-lg mx-3 text-[#71b1d1]  hover:bg-gray-300'>Sign up</button>
+            <Link href={'/login'}>
+            <button className='bg-[#f5f5f7] w-max h-10 px-4  rounded-lg mx-3 text-[#71b1d1]  hover:bg-gray-300'>Login</button></Link>
+            <Link href={'/register'}>
+            <button className='bg-[#f5f5f7] w-max h-10 px-4  rounded-lg mx-3 text-[#71b1d1]  hover:bg-gray-300'>Sign up</button></Link>
         </div>
     </main> 
     </>
---
+++ client/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+     <main className='bg-[#71B1D1] h-screen w-screen'>
+        {/* Form */}
+        <div className='bg-[#f5f5f7] w-[25vw] h-max py-2'>
+
+        </div>
+    </main> 
+    </>
+  )
+}
