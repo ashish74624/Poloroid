76184c563632fdf3e5c1eef0266fec6447be7b04
Ashish <ashish74624@gmail.com>
2023-06-20 16:42:40 +0530

    Mongo db connection verification added

--- client/app/Components/Post.tsx
+++ client/app/Components/Post.tsx
@@ -18,12 +18,15 @@ interface PostProps{
 export default function Post({id,firstName,lastName,image,caption}:PostProps) {
   return (
     <>
-       <div className='relative w-[22vw] h-[58vh] px-6 pt-5 flex flex-col items-center bg-[#fffff2] border-2 border-[#1d1d1f]'>
+    <div className='bg-[#71B1D1] w-max h-max px-6 pb-6 pt-3 rounded'>
+      <p className='pl-1 mb-1 hover:underline cursor-pointer w-max'>{firstName} {lastName}</p>
+       <div className='relative w-[22vw] h-[58vh] px-6 pt-5 flex flex-col items-center bg-white border border-[#1d1d1f] shadow-xl'>
                 <Image className='w-[297px] h-[347px]' src={image} alt='Hello' width={100} height={100} />
                 <div className="">
-                <p className={` ${ks.className} w-[21.8vw] h-16 px-4 bg-[#f3f3ed] flex justify-center items-center`}>{caption}</p>
+                <p className={` ${ks.className} w-[21.8vw] h-16 px-4 bg-white flex justify-center items-center`}>{caption}</p>
                 </div>
             </div>
+    </div>
     </>
   )
 }
