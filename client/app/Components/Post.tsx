0f9650a3efb08ddf00e9cac9d7500f526e332d67
Ashish <ashish74624@gmail.com>
2023-06-23 23:25:11 +0530

    In progress

--- client/app/Components/Post.tsx
+++ client/app/Components/Post.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { Kaushan_Script } from 'next/font/google';
 import { Amatic_SC } from 'next/font/google';
 import PostSkel from './PostSkel';
+import userDefaultImage from '@/public/userDefaultImage.webp'
 
 const ks = Kaushan_Script({
     subsets:['latin'],
@@ -47,46 +48,33 @@ interface Post {
 export default async function Post({userImg,email}:any) {
     const [posts, setPosts] = useState<Post[]>([]);
     const [data, setData] = useState<Post[]>([]);
+    const [go,setGo] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [isDataLoading,setIsDataLoading] = useState(true);
 
-    // const pstdt = getPostData(email);
-    // // const imgdt = getPostImage(email);
-    // const entirePostData =  getAllData(email);
-
-    // const [postData, entirePost] = await Promise.all([pstdt,entirePostData])
-
     useEffect(()=>{
-      const getPostData = async()=>{
-        try{
-          let res = await fetch(`http:localhost:3001/postsData/${email}`);
-          let data =await res.json();
-          setData(data);
-          setIsDataLoading(false);
-        }catch(err){
-          setIsDataLoading(false);
-          console.log("Unable to fetch data");
-        }
+      const getPostData=async()=>{
+        let res = await fetch(`http://localhost:3001/postsData/${email}`);
+        
+          let data = await res.json()
+        
+        setPosts(data);
+        setIsDataLoading(false)
       }
       getPostData();
-    },[email])
-    useEffect(() => {
-        const getPosts = async () => {
-          try {
-            if(email){
-                const response = await fetch(`http://localhost:3001/posts/${email}`);
-                const articles = await response.json();
-                setPosts(articles);
-                setIsLoading(false)
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        };
-  
-        getPosts();
-      }, [email]);
-    
+
+    },[email]);
+    useEffect(()=>{
+     const getData=async()=>{
+      let res = await fetch(`http://localhost:3001/posts/${email}`); 
+      let data = await res.json();
+      setPosts(data);
+      setIsLoading(false);
+
+    }
+    getData();
+   },[]);
+ 
     
   return (
     <>
@@ -98,36 +86,6 @@ export default async function Post({userImg,email}:any) {
         </>
         ):
         (
-        <>
-        {
-          isLoading?
-          (
-          <>
-          {data.map((post)=>{
-            return(
-            <>
-            <div className='my-5'>
-    <div key={post._id} className='bg-[#71B1D1] w-max h-max px-6 pt-3 pb-4 rounded-t '>
-      <div className='flex space-x-3 mb-4'>
-        <Image className='w-8 h-8 rounded-full' src={userImg} alt='User Profile' width={100} height={100}/>
-        <p className='text-white mb-1 hover:underline cursor-pointer w-max'>{post.firstName || 'John'} {post.lastName || 'Doe'}</p>
-      </div>
-      <div className='relative w-[22vw] h-[58vh] px-6 pt-5 flex flex-col items-center bg-white   border border-[#1d1d1f] shadow-xl'>
-                <div className='w-[297px] h-[347px] bg-gray-500 animate-pulse'>Loading...</div>
-                <div className="">
-                <p className={` ${ks.className} w-[21.8vw] h-16 px-4 bg-white flex justify-center items-center`}>{post.caption}</p>
-                </div>
-      </div>
-    </div>
-      <button className={`${as.className} text-3xl w-[386px] rounded-b py-1 h-max text-[#F8C732] bg-[#71B1D1] hover:bg-[#77a4bc] transition duration-300`}>Like</button>
-    </div>
-            </>
-            )
-        })}
-          </>
-          )
-          :
-          (
           <>
           {posts.map((post)=>{
             return(
@@ -139,7 +97,7 @@ export default async function Post({userImg,email}:any) {
         <p className='text-white mb-1 hover:underline cursor-pointer w-max'>{post.firstName || 'John'} {post.lastName || 'Doe'}</p>
       </div>
       <div className='relative w-[22vw] h-[58vh] px-6 pt-5 flex flex-col items-center bg-white   border border-[#1d1d1f] shadow-xl'>
-                <Image className='w-[297px] h-[347px] overflow-hidden' src={post.image} alt='Hello' width={100} height={100} />
+                <Image className='w-[297px] h-[347px] overflow-hidden' src={post.image || userDefaultImage} alt='Hello' width={100} height={100} />
                 <div className="">
                 <p className={` ${ks.className} w-[21.8vw] h-16 px-4 bg-white flex justify-center items-center`}>{post.caption}</p>
                 </div>
@@ -151,11 +109,8 @@ export default async function Post({userImg,email}:any) {
             )
         })}
           </>
-          )
+        )
         }
         </>
         )
-      }
-    </>    
-  )
-}
+      }  
