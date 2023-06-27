a91b52193a0aa6ac8485f0e0bf9709f786d4d191
Ashish <ashish74624@gmail.com>
2023-06-27 15:39:32 +0530

    Changed the color scheme from 71B1D1 to 58b8e8

--- client/app/Components/Sidebar.tsx
+++ client/app/Components/Sidebar.tsx
@@ -5,8 +5,8 @@ export default function Sidebar() {
     <>
       <main className=' overflow-hidden  '>
         <ul className='bg-[#F8F8F8] fixed z-10 h-[94vh] w-[17vw] border-r border-gray-600 flex flex-col items-center space-y-2 pt-4'>
-            <li className='px-4 py-2 bg-[#71B1D1] text-white rounded-lg w-[15vw] font-serif font-medium flex justify-center  text-lg'>Home</li>
-            <li className='px-4 py-2 bg-[#71B1D1] bg-opacity-60 text-white rounded-lg w-[15vw] font-serif font-medium flex justify-center  text-lg'>Notifications</li>
+            <li className='px-4 py-2 bg-[#58b8e8] text-white rounded-lg w-[15vw] font-serif font-medium flex justify-center  text-lg'>Home</li>
+            <li className='px-4 py-2 bg-[#58b8e8] bg-opacity-60 text-white rounded-lg w-[15vw] font-serif font-medium flex justify-center  text-lg'>Notifications</li>
         </ul>
       </main>
     </>
