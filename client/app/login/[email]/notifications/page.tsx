3e90da7a54eda624232fa31f9b8c30e69e165c0b
Ashish <ashish74624@gmail.com>
2023-07-06 22:13:23 +0530

    Notification fallback added

--- client/app/login/[email]/notifications/page.tsx
+++ client/app/login/[email]/notifications/page.tsx
@@ -16,25 +16,44 @@ export default async function Notifications({params:{email}}:Params) {
     
   const res = await fetch(`http://localhost:3001/notifications/${email}`);
   const data = await res.json();
-  const notification = data.msg;
+  if(data.status==='ok'){
+    const notification = data.msg;
 
-  return (
-    <section className=' h-screen w-screen bg-[#F8F8F8]'>
-      <Navbar navData={false}/>
-      <div className='mt-16'>
-      <Sidebar home={'bg-opacity-60'} notifications={'bg-opacity-100'} email={email}/>
-      </div>
-      <div className='ml-[19vw] w-[50vw] '>
-        <Suspense fallback={<NotfSkel/>}>
-          {
-              notification.map((data:any)=>(
-                <>
-                <NotfCard friendName={data.sender.name} friendImage={data.sender.profilePicture || userDefaultImage}/>
-              </>
-              ))
-          }    
-        </Suspense>
-      </div>
-    </section>
+    return (
+      <section className=' h-screen w-screen bg-[#F8F8F8]'>
+        <Navbar navData={false}/>
+        <div className='mt-16'>
+        <Sidebar home={'bg-opacity-60'} notifications={'bg-opacity-100'} email={email}/>
+        </div>
+        <div className='ml-[19vw] w-[50vw] '>
+          <Suspense fallback={<NotfSkel/>}>
+            {
+                notification.map((data:any)=>(
+                  <>
+                  <NotfCard friendName={data.sender.name} friendImage={data.sender.profilePicture || userDefaultImage}/>
+                </>
+                ))
+            }    
+          </Suspense>
+        </div>
+      </section>
+    )
+  }
+else{
+  return(
+    <>
+      <section className=' h-screen w-screen bg-[#F8F8F8]'>
+        <Navbar navData={false}/>
+        <div className='mt-16'>
+        <Sidebar home={'bg-opacity-60'} notifications={'bg-opacity-100'} email={email}/>
+        </div>
+        <div className='ml-[19vw] w-[50vw] h-[75vh] flex justify-center items-center'>
+          You Currently have no Notifications
+        </div>
+      </section>
+    </>
   )
 }
+
+  }
+ 
