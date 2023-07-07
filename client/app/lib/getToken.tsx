0e6fa7c1942726abb8c29256c9edbae63f97f000
Ashish <ashish74624@gmail.com>
2023-07-07 22:12:43 +0530

    Deleted useless files

--- client/app/lib/getToken.tsx
+++
@@ -1,7 +0,0 @@
-export default function getToken() {
-  if (typeof window !== 'undefined') {
-    const item = localStorage.getItem('token');
-    return item ? String(item) : null; // Return token as a string or null
-  }
-  return null; // Return null if window is undefined (e.g., during server-side rendering)
-}
