cb596690fed3696f4b7b857f711e1714fc3ae911
Ashish <ashish74624@gmail.com>
2023-07-17 00:57:02 +0530

    Loading toast added to login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Comfortaa } from 'next/font/google'
+import toast , {Toaster}  from 'react-hot-toast'
 
 const Comf = Comfortaa({
   subsets:['cyrillic'],
@@ -32,6 +33,9 @@ export default function Login() {
         }
       )
     });
+
+    const loadingToast =()=> toast.loading("loading");
+    loadingToast();
   
     const data = await response.json();
     console.log(data.status);
@@ -50,26 +54,25 @@ export default function Login() {
      <main className='bg-[#58b8e8] h-screen w-screen flex flex-col pt-32 items-center space-y-8'>
       <h3 className={` ${Comf.className} text-white text-5xl`}>Welcome Back</h3>
         {/* Form */}
-        <div className='bg-white w-[350px] md:w-96 h-max py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
-          
-<form onSubmit={handleLogin}>
-  <div className="relative z-0 w-full mb-6 group ">
-      <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-      onChange={(e)=>{setEmail(e.target.value)}}
-      />
-      <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-  </div>
-  <div className="relative z-0 w-full mb-6 group">
-      <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-      onChange={(e)=>{setPassword(e.target.value)}}
-      />
-      <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-  </div>
-  <button type="submit" className="text-white bg-[#F8C732] hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
-  <p className='text-black text-xs mt-4'>Don't have an account yet ?<Link href={'/register'}><span className='text-xs text-[#F8C732] hover:underline pl-1'>Sign up</span></Link> </p>
-</form>
-
-        </div>
+      <div className='bg-white w-[350px] md:w-96 h-max py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
+        <form onSubmit={handleLogin}>
+          <div className="relative z-0 w-full mb-6 group ">
+            <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+            onChange={(e)=>{setEmail(e.target.value)}}
+            />
+            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+            onChange={(e)=>{setPassword(e.target.value)}}
+            />
+            <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+          </div>
+          <button type="submit" className="text-white bg-[#F8C732] hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
+          <p className='text-black text-xs mt-4'>Don't have an account yet ?<Link href={'/register'}><span className='text-xs text-[#F8C732] hover:underline pl-1'>Sign up</span></Link> </p>
+        </form>
+      </div>
+      <Toaster/>
     </main> 
     </>
   )
