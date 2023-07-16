14eab7a140d04eb089d69c040e2dc4d8196d2944
Ashish <ashish74624@gmail.com>
2023-07-16 21:00:15 +0530

    Login and register page made responsive

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -60,22 +60,22 @@ export default function Register() {
   return (
     <>
       <main className='bg-[#58b8e8] h-screen w-screen flex flex-col pt-7 items-center space-y-5'>
-      <h3 className='text-white text-5xl'>Welcome to <span className={`${Comf.className}`}>polaroid</span></h3>
-      <div className='bg-white w-[25vw] h-max py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
+      <h3 className=' text-white text-4xl lg:text-5xl'>Welcome to <span className={`${Comf.className}`}>polaroid</span></h3>
+      <div className='bg-white w-80 lg:w-96 h-max py-4 pb-5 lg:pb-0  lg:py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
         
 <form onSubmit={handleRegister}>
   <div>
   {file?
             (<>
             <div className='w-full items-center flex flex-col'>
-                <Image className='w-36 h-36 rounded-full' src={file} alt='Hello' width={100} height={100} />
+                <Image className=' w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 rounded-full' src={file} alt='Hello' width={100} height={100} />
                 <button onClick={()=>{setFile('')}} className='my-2 hover:text-red-500 transition duration-200'>Profile Picture</button>
             </div>
             </>)
             :
             (<>
             <div className='w-full items-center flex flex-col '>
-                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-36 h-36 rounded-full  cursor-pointer bg-gray-400/70">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 rounded-full  cursor-pointer bg-gray-400/70">
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg width="6rem" height="6rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M8 9C8 6.79086 9.79086 5 12 5C14.2091 5 16 6.79086 16 9C16 11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9ZM15.8243 13.6235C17.1533 12.523 18 10.8604 18 9C18 5.68629 15.3137 3 12 3C8.68629 3 6 5.68629 6 9C6 10.8604 6.84668 12.523 8.17572 13.6235C4.98421 14.7459 3 17.2474 3 20C3 20.5523 3.44772 21 4 21C4.55228 21 5 20.5523 5 20C5 17.7306 7.3553 15 12 15C16.6447 15 19 17.7306 19 20C19 20.5523 19.4477 21 20 21C20.5523 21 21 20.5523 21 20C21 17.2474 19.0158 14.7459 15.8243 13.6235Z" stroke="#000000" strokeWidth="0.001" />
@@ -88,32 +88,32 @@ export default function Register() {
             </>)}
   </div>
 <div className="grid md:grid-cols-2 md:gap-6">
-    <div className="relative z-0 w-full mb-6 group">
+    <div className="relative z-0 w-full mb-3 md:mb-6 group">
         <input type="text" name="first_name" id="first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
         onChange={(e)=>{setFirstName(e.target.value)}}
         />
         <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
     </div>
-    <div className="relative z-0 w-full mb-6 group">
+    <div className="relative z-0 w-full mb-3 md:mb-6 group">
         <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
         onChange={(e)=>{setLastName(e.target.value)}}
         />
         <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
     </div>
   </div>
-  <div className="relative z-0 w-full mb-6 group">
+  <div className="relative z-0 w-full mb-3 md:mb-6 group">
       <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none    focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
       onChange={(e)=>{setEmail(e.target.value)}}
       />
       <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
   </div>
-  <div className="relative z-0 w-full mb-6 group">
+  <div className="relative z-0 w-full mb-3 md:mb-6 group">
       <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
       onChange={(e)=>{setPassword(e.target.value)}}
       />
       <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
   </div>
-  <div className="relative z-0 w-full mb-6 group">
+  <div className="relative z-0 w-full mb-3 md:mb-6 group">
       <input type="text" name="place" id="place" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
       onChange={(e)=>{setPlace(e.target.value)}}
       />
