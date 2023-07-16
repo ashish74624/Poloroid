234db7dc390add7e9fd4e765142024279d53b796
Ashish <ashish74624@gmail.com>
2023-07-16 21:00:15 +0530

    Login and register page made responsive

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -50,7 +50,7 @@ export default function Login() {
      <main className='bg-[#58b8e8] h-screen w-screen flex flex-col pt-32 items-center space-y-8'>
       <h3 className={` ${Comf.className} text-white text-5xl`}>Welcome Back</h3>
         {/* Form */}
-        <div className='bg-white w-[25vw] h-max py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
+        <div className='bg-white w-[350px] md:w-96 h-max py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
           
 <form onSubmit={handleLogin}>
   <div className="relative z-0 w-full mb-6 group ">
