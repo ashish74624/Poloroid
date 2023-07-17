adb9dd70691057ee9b4e71ebda620005bebca569
Ashish <ashish74624@gmail.com>
2023-07-18 00:17:28 +0530

    Homepage loading ui added

--- client/app/home/[email]/loading.tsx
+++ client/app/home/[email]/loading.tsx
@@ -19,14 +19,14 @@ export default function loading() {
       <nav>
         <NavbarSkel/>
       </nav>
-      <section className=' h-[91vh] w-screen grid grid-cols-5'>
-        <div className=' border-r border-gray-600'><Sidebar /></div>
-        <div className=' col-span-3 overflow-x-hidden overflow-y-scroll flex flex-col items-center'>
+      <section className=' h-[91vh] w-screen md:grid flex md:grid-cols-4 lg:grid-cols-5'>
+        <div className=' border-r border-gray-600 md:block hidden'><Sidebar /></div>
+        <div className=' w-screen md:w-[80vw] lg:w-[60vw] md:col-span-3 overflow-x-hidden overflow-y-scroll flex flex-col items-center'>
             <PostSkel/>
         </div>
-        <div className='border-l border-gray-600 overflow-x-hidden overflow-y-scroll'>
+        <div className=' lg:block hidden border-l border-gray-600 overflow-x-hidden overflow-y-scroll'>
         <ul className=' overflow-y-scroll  flex flex-col items-center space-y-2 pt-4 right-0'>
-            <h3 className='text-xl mb-2'>People you may know</h3>
+            <h3 className='text-base lg:text-xl mb-2'>People you may know</h3>
             <UserSkel/>
             <UserSkel/>
             <UserSkel/>
