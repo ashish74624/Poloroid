248eb4999039a2f7679b585da5b54372a7278b6f
Ashish <ashish74624@gmail.com>
2023-07-17 21:39:08 +0530

    Notifications page made responsive

--- client/app/Components/NotfCard.tsx
+++ client/app/Components/NotfCard.tsx
@@ -37,22 +37,22 @@ export default async function NotfCard({friendImage,friendName,email,friendID}:a
     
         return (
             <>
-              <section className='bg-white h-16 my-3 w-[50vw] rounded-lg shadow flex items-center justify-between'>
-                <span className='flex ml-4'>
-                    <img className='w-11 h-11 rounded-full' src={`https://res.cloudinary.com/dcgjy3xv7/image/upload/v1687762741/${friendImage}`} alt='Notification' height={100} width={100}/>
-                    <h4 className='pt-[0.375rem] pl-2 text-[#F8C732] font-bold text-lg'>{friendName}</h4>
-                    <p className='pt-2 pl-2 font-normal'>wants to be your friend</p>
+              <section className='bg-white h-32 md:h-16 my-3 w-[95vw] md:w-[80vw] lg:w-[50vw] rounded-lg shadow flex md:flex-row flex-col md:items-center md:justify-between'>
+                <span className='flex ml-4 md:py-0 py-3'>
+                    <img className=' w-14 h-14 md:w-11 md:h-11 rounded-lg md:rounded-full' src={`https://res.cloudinary.com/dcgjy3xv7/image/upload/v1687762741/${friendImage}`} alt='Notification' height={100} width={100}/>
+                    <h4 className='pt-[0.375rem] pl-2 text-[#F8C732] font-bold text-2xl md:text-lg'>{friendName}</h4>
+                    <p className='pt-2 pl-2 font-normal text-xl md:text-base'>wants to be your friend</p>
                 </span>
-                <span className='space-x-4 mr-4'>
-                    <button className='bg-[#58b8e8] hover:bg-[#4cc3ff] px-7 text-white py-2 rounded-lg transition duration-300'
+                <div className='space-x-4 md:mr-4 mx-auto w-max'>
+                    <button className='bg-[#58b8e8] hover:bg-[#4cc3ff] px-4 lg:px-7 text-white py-2 md:py-1 lg:py-2 rounded-lg transition duration-300 w-[40vw] md:w-max'
                     onClick={()=>{handleAddFriend()}}
                     >
                         Accept
                     </button>
-                    <button className='bg-red-500 hover:bg-red-700 transition duration-300 px-7 text-white py-2 rounded-lg'
+                    <button className='bg-red-500 hover:bg-red-700 transition duration-300 px-4 lg:px-7 text-white py-2 md:py-1 lg:py-2 rounded-lg w-[40vw] md:w-max'
                     onClick={()=>{rejectRequest() }}
                     >Reject</button>
-                </span>
+                </div>
               </section>
             </>
           )
