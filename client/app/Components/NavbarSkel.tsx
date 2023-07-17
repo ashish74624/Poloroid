35015c6019eca3ce25802d6c1013750fe9d8a700
Ashish <ashish74624@gmail.com>
2023-07-18 00:17:28 +0530

    Homepage loading ui added

--- client/app/Components/NavbarSkel.tsx
+++ client/app/Components/NavbarSkel.tsx
@@ -16,15 +16,18 @@ export default function NavbarSkel() {
     <>
       {/* Navbar Start */}
       <nav className=' border-b border-gray-600 bg-[#F8F8F8] w-screen'>
-      <div className={`w-[80vw] h-[9vh]  flex justify-between  mx-auto`}>
+      <div className={`w-[80vw] h-[8vh] lg:h-[9vh]  flex justify-between  mx-auto items-center`}>
         
-          <span className={`${Com.className}  text-3xl  bg-clip-text text-[#58b8e8] mt-3`}>
+          <span className={`${Com.className}  text-3xl  bg-clip-text text-[#58b8e8]`}>
             polaroid
             </span>
           <span className=' flex '>
-          <Image className="w-12 h-12 rounded-full border-2 mt-2 border-[#F8C732] p-1 mr-3" src={ userDefaultImage} alt ={"Helo"} width={100} height={100}/>
-          <button>
-            <MyIcon/>
+          <Image className="w-10 h-10 lg:w-12 lg:h-12 rounded-full border-2 border-[#F8C732] p-1 mr-3" src={ userDefaultImage} alt ={"Helo"} width={100} height={100}/>
+          <button disabled className="flex items-center justify-center bg-[#F8C732] text-white font-bold w-10 h-10 lg:w-12 lg:h-12 rounded-full ">
+      <svg className="h-6 w-6 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path>
+      </svg>
+
           </button>
         </span>
         </div>
