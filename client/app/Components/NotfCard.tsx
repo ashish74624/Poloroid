833b237fc6752fe49754ee1d63365a089e69098d
Ashish <ashish74624@gmail.com>
2023-07-17 22:22:40 +0530

    FAQ page made responsive

--- client/app/Components/NotfCard.tsx
+++ client/app/Components/NotfCard.tsx
@@ -37,7 +37,7 @@ export default async function NotfCard({friendImage,friendName,email,friendID}:a
     
         return (
             <>
-              <section className='bg-white h-32 md:h-16 my-3 w-[95vw] md:w-[80vw] lg:w-[50vw] rounded-lg shadow flex md:flex-row flex-col md:items-center md:justify-between'>
+              <section className='bg-white h-32 md:h-16 my-3 w-[95vw] md:w-[80vw] lg:w-[60vw] rounded-lg shadow flex md:flex-row flex-col md:items-center md:justify-between'>
                 <span className='flex ml-4 md:py-0 py-3'>
                     <img className=' w-14 h-14 md:w-11 md:h-11 rounded-lg md:rounded-full' src={`https://res.cloudinary.com/dcgjy3xv7/image/upload/v1687762741/${friendImage}`} alt='Notification' height={100} width={100}/>
                     <h4 className='pt-[0.375rem] pl-2 text-[#F8C732] font-bold text-2xl md:text-lg'>{friendName}</h4>
