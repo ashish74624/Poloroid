0b3a003c66ec09d8435f9fde7caac81e9b560196
Ashish <ashish74624@gmail.com>
2023-07-19 03:07:29 +0530

    Register page bug solved

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -25,6 +25,7 @@ export default function Register() {
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const [place,setPlace] = useState('')
+  const [form,setForm] = useState(true);
   const router = useRouter();
 
   const handleImageSelect = async(event:any) => {
@@ -68,14 +69,82 @@ export default function Register() {
       }
     }
 
+    const handleNextButtonClick =async () => {
+      if (!firstName || !lastName || !email || !password || !place) {
+        return;
+      }
+    
+      // Check if the email is in a valid format
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailPattern.test(email)) {
+        toast.error('Please enter a valid email address.');
+        return;
+      }
+    
+      // If all required fields are filled and email is valid, move to the next part
+      setForm(false);
+    }
+
+    const handleBackButton=()=>{
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setPassword('');
+      setPlace('')
+      setForm(true);
+    }
+
   return (
     <>
-      <main className='bg-[#58b8e8] h-screen w-screen flex flex-col pt-7 items-center space-y-5'>
+      <main className='bg-[#58b8e8] h-screen w-screen flex flex-col pt-10 items-center space-y-10'>
       <h3 className=' text-white text-4xl lg:text-5xl'>Welcome to <span className={`${Comf.className}`}>polaroid</span></h3>
-      <div className='bg-white w-80 lg:w-96 h-max py-4 pb-5 lg:pb-8  lg:py-8 rounded-xl px-8 shadow-xl shadow-gray-600'>
+      <div className='bg-white w-80 lg:w-96 h-max py-4 pb-5 lg:pb-8  lg:py-8 rounded-xl px-8 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
         
-<form onSubmit={handleRegister}>
-  <div>
+      <form onSubmit={handleRegister}>
+        {
+          form ?
+          <>
+            <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-3 md:mb-6 group">
+                <input type="text" name="first_name" id="first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+                onChange={(e)=>{setFirstName(e.target.value)}}
+                />
+                <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+            </div>
+            <div className="relative z-0 w-full mb-3 md:mb-6 group">
+                <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+                onChange={(e)=>{setLastName(e.target.value)}}
+                />
+                <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+            </div>
+          </div>
+          <div className="relative z-0 w-full mb-3 md:mb-6 group">
+              <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none    focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+              onChange={(e)=>{setEmail(e.target.value)}}
+              />
+              <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+          </div>
+          <div className="relative z-0 w-full mb-3 md:mb-6 group">
+              <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+              onChange={(e)=>{setPassword(e.target.value)}}
+              />
+              <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+          </div>
+          <div className="relative z-0 w-full mb-3 md:mb-6 group">
+              <input type="text" name="place" id="place" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
+              onChange={(e)=>{setPlace(e.target.value)}}
+              />
+              <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
+          </div>
+          <button 
+          onClick={handleNextButtonClick}
+          className="text-white bg-[#F8C732]  hover:bg-yellow-500  focus:outline-none focus:ring-yellow-500 focus:ring-2 active:bg-white active:text-[#F8C732] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+            Next
+          </button>
+          </>
+          :
+          <>
+          <div>
   {file?
             (<>
             <div className='w-full items-center flex flex-col'>
@@ -85,6 +154,7 @@ export default function Register() {
             </>)
             :
             (<>
+            <h2 className='w-full flex justify-center text-lg font-sans mb-4'>Please choose an User Profile</h2>
             <div className='w-full items-center flex flex-col '>
                 <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-28 h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 rounded-full  cursor-pointer bg-gray-400/70">
                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -97,44 +167,23 @@ export default function Register() {
             <span className='my-2'>Profile Picture</span>
             </div>
             </>)}
-  </div>
-<div className="grid md:grid-cols-2 md:gap-6">
-    <div className="relative z-0 w-full mb-3 md:mb-6 group">
-        <input type="text" name="first_name" id="first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-        onChange={(e)=>{setFirstName(e.target.value)}}
-        />
-        <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-    </div>
-    <div className="relative z-0 w-full mb-3 md:mb-6 group">
-        <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-        onChange={(e)=>{setLastName(e.target.value)}}
-        />
-        <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
-    </div>
-  </div>
-  <div className="relative z-0 w-full mb-3 md:mb-6 group">
-      <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none    focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-      onChange={(e)=>{setEmail(e.target.value)}}
-      />
-      <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-  </div>
-  <div className="relative z-0 w-full mb-3 md:mb-6 group">
-      <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-      onChange={(e)=>{setPassword(e.target.value)}}
-      />
-      <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-  </div>
-  <div className="relative z-0 w-full mb-3 md:mb-6 group">
-      <input type="text" name="place" id="place" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#71B1D1] peer" placeholder=" " required
-      onChange={(e)=>{setPlace(e.target.value)}}
-      />
-      <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#71B1D1]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
-  </div>
-  
-  <button type="submit" className="text-white bg-[#F8C732]  hover:bg-yellow-500  focus:outline-none focus:ring-yellow-500 focus:ring-2 active:bg-white active:text-[#F8C732] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
-  <p className='text-black text-xs mt-4'>Already have an account ?<Link href={'/login'}><span className='text-xs text-[#F8C732] hover:underline pl-1'>Login</span></Link> </p>
-</form>
+          </div>
+          <span className='space-x-4 w-full flex justify-between'>
+            <button type="submit" className="text-white bg-[#F8C732]  hover:bg-yellow-500  focus:outline-none focus:ring-yellow-500 focus:ring-2 active:bg-white active:text-[#F8C732] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+            <button
+            onClick={handleBackButton}
+            className="text-white bg-[#F8C732]  hover:bg-yellow-500  focus:outline-none focus:ring-yellow-500 focus:ring-2 active:bg-white active:text-[#F8C732] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Back</button>
+          </span>
+          
+          </>
+        }
+        
 
+          
+  
+          
+      </form>
+      <p className='text-black text-xs mt-4'>Already have an account ?<Link href={'/login'}><span className='text-xs text-[#F8C732] hover:underline pl-1'>Login</span></Link> </p>
       </div>
       <Toaster/>
       </main>
