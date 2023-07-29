2461667eaa4e12ec572769471f65b34d4fa7871f
Ashish <ashish74624@gmail.com>
2023-07-30 03:22:26 +0530

    Fixed the BottomNav issue

--- client/app/Components/BottomNav.tsx
+++ client/app/Components/BottomNav.tsx
@@ -86,7 +86,7 @@ export default function BottomNav({firstName,lastName,email,upload,people}:any) 
     <main className='fixed w-screen z-50 bottom-0 bg-[#F8F8F8] py-2 flex justify-center border-t border-gray-600'>
     <div className=' px-4  w-max rounded-full'>
         <div className='flex w-60 h-12 py-2  items-center justify-between'>
-              <button onClick={()=>{setSide(false);setVisible(visible);}}>
+              <button onClick={()=>{setSide(false);setVisible(false);}}>
             <Link href={`/home/${email}`}>
                 <span className=''><HomeIcon/></span>
                 <p className=' text-xs'>Home</p>
