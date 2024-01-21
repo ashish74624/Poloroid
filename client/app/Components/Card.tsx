38d5b312a3de6f75412a31b5de2c8e17fdfb211d
Ashish <ashish74624@gmail.com>
2024-01-22 02:50:02 +0530

    Increased liking speed on posts

--- client/app/Components/Card.tsx
+++ client/app/Components/Card.tsx
@@ -26,7 +26,9 @@ const kst = Kalam({
 
 export default function Card({id,firstName,lastName,email,userProfile,likes,likedBy,image,caption,userID}:PostProps) {
     const [like,setLike]:[number, React.Dispatch<React.SetStateAction<number>>] = useState(likes);
-
+    console.log(likedBy);
+    console.log(userID);
+    const [isLiked,setIsLiked] = useState(likedBy.some(obj=> obj.id===userID ));
     const debouncing = (func: any, delay: number) => {
       let timer: ReturnType<typeof setTimeout>;
       return function (this: any, ...args: any[]) {
@@ -35,7 +37,7 @@ export default function Card({id,firstName,lastName,email,userProfile,likes,like
       };
     };
 
-    const handleLike=  debouncing(async (id: any) => {
+    const handleLike=  async (id: any) => {
       const response = await fetch(`${backendURL}/like/${id}`, {
         method: 'PUT',
         headers: {
@@ -45,23 +47,50 @@ export default function Card({id,firstName,lastName,email,userProfile,likes,like
           emailOfUser: email,
         }),
       });
-      const data = await response.json();
-      if (response.ok) {
-        if (data.msg === 'liked') {
-          setLike((prev: number) => prev + 1);
-        }
-        if (data.msg === 'disliked') {
-          setLike((prev: number) => prev - 1);
-        }
-      } else {
-        setLike(like);
+    }
+
+    function toggleLiking() {
+      if(isLiked){
+        setLike(prev=>prev-1);
+        handleLike(id);
+        setIsLiked(false)
+      }else{
+        setLike(prev=>prev+1);
+        handleLike(id);
+        setIsLiked(true)
       }
-    }, 1000); // Debounce delay of 1000ms (1 second)
-        
+    }
+
     
   return (
     <>
-          <section className='md:pb-0 pb-2'>
+      {isLiked 
+      ?
+      <section className='md:pb-0 pb-2'>
+            <section key={id} className="bg-[#58b8e8] w-[360px] my-2 md:w-[400px] h-max flex flex-col rounded-md  items-center">
+            <span className="flex space-x-2 mx-auto py-2 items-center w-[310px] md:w-[360px]">
+                <Image
+                src={`https://res.cloudinary.com/dcgjy3xv7/image/upload/v1688970909/${userProfile}`}
+                className="w-10 h-10 rounded-full" alt='userImage' width={100} height={100}
+                />
+                <h5 className="text-white text-lg font-serif">{firstName} {lastName}</h5>
+            </span>
+            <div className=" bg-white h-max w-[320px] md:w-[360px] border border-black">
+                <Image
+                src={`https://res.cloudinary.com/dcgjy3xv7/image/upload/v1687762741/${image}`} className="md:w-[300px] w-[270px] h-96 mx-auto mt-[25px] md:mt-[30px]" alt ='Image' width={500} height={500} />
+                <p className={` ${kst.className} md:w-[300px] w-[260px] mx-auto py-3 md:py-5 flex justify-center items-center`}>
+                  {caption}
+                </p>
+            </div>
+              <button className= {`font-serif rounded-b-md flex justify-center items-center flex-grow py-3 hover:text-[#F8C732] text-yellow-500  transition duration-300 bg-[#58b8e8] text-2xl w-[360px] md:w-[400px]  focus:outline-[#F8C732] `}
+             onClick={() => { toggleLiking();  }}
+            >
+                Like {like}
+            </button>
+        </section>
+        </section>
+      :
+      <section className='md:pb-0 pb-2'>
             <section key={id} className="bg-[#58b8e8] w-[360px] my-2 md:w-[400px] h-max flex flex-col rounded-md  items-center">
             <span className="flex space-x-2 mx-auto py-2 items-center w-[310px] md:w-[360px]">
                 <Image
@@ -78,12 +107,14 @@ export default function Card({id,firstName,lastName,email,userProfile,likes,like
                 </p>
             </div>
               <button className= {`font-serif rounded-b-md flex justify-center items-center flex-grow py-3 hover:text-[#F8C732] text-white  transition duration-300 bg-[#58b8e8] text-2xl w-[360px] md:w-[400px]  focus:outline-[#F8C732] `}
-             onClick={() => handleLike(id)}
+             onClick={() => {toggleLiking()}}
             >
                 Like {like}
             </button>
         </section>
         </section>
+      }
+          
     </>
   )
 }
