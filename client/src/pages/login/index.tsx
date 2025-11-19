da6662297f98dec09fbe9ea5333c4f2c5590eb35
Ashish <ashish74624@gmail.com>
2025-11-20 00:06:10 +0530

    changed navigation after login

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -42,7 +42,7 @@ export default function Login() {
                     onSuccess: (data) => {
                         setEmail(formData.email)
                         localStorage.setItem("token", data.access)
-                        navigate("/profile");
+                        navigate("/home");
                     },
                 }
             );
