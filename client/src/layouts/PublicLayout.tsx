073909725e293d8c8919152c445d0a21535ae44a
Ashish <ashish74624@gmail.com>
2026-01-25 01:52:12 +0530

    sign up overflow fix

--- client/src/layouts/PublicLayout.tsx
+++ client/src/layouts/PublicLayout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "react-hot-toast"
 
 export default function PublicLayout() {
     return (
-        <section className="bg-[#FCFBF9] h-screen w-screen">
+        <section className="bg-[#FCFBF9] h-screen w-screen overflow-y-auto overflow-x-clip">
             <Outlet />
             <Toaster />
         </section>
