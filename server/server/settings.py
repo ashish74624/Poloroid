0710ea510ab3ed54082099f2fd3c02f14f5b877d
Ashish <ashish74624@gmail.com>
2026-02-12 01:31:43 +0530

    settings change again

--- server/server/settings.py
+++ server/server/settings.py
@@ -56,13 +56,13 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-
-CORS_ALLOWED_ORIGINS = [...]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://poloroid.vercel.app",
+    "http://localhost:5173",
+]
 
 
 ROOT_URLCONF = "server.urls"
